c0999e286040ff7c20ad68406293184d565362bb
Timo Halbesma <timohalbesma@gmail.com>
2017-05-08 22:57:15 +0200

    Allow only one instance to be created

--- apiweb/apps/main/models.py
+++ apiweb/apps/main/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from django.core.exceptions import ValidationError
+
+def validate_only_one_instance(obj):
+    model = obj.__class__
+    if (model.objects.count() > 0 and obj.id != model.objects.get().id):
+        raise ValidationError("Errror: only 1 instance of {0} is allowed and it already exists.".format(model.__name__))
 
 class ContactInfo(models.Model):
-	secretary_email_adres = models.EmailField(_('Secretary mail adres of the API'), default='secr-astro-science@uva.nl')
-	adres_api = models.CharField(_('Adres of the API institute'), max_length=256, default='Sciencepark 904, 1098XH, Amsterdam')
-	postbox_adres_api = models.CharField(_('Postbox adres of the API institute'),max_length=256, default='PO Box 94249, 1090 GE Amsterdam')
-	telephone_api = models.CharField(_('Telephone number of the API'), default='0031205257491', max_length=20)
-	webmaster_email_adres = models.EmailField(_('Webmaster mail adres'),default='secr-astro-science@uva.nl')
+    secretary_email_address = models.EmailField(_("API Secretariat e-mail address"), default="secr-astro-science@uva.nl")
+    address_api = models.CharField(_("API Address"), max_length=256, default="Sciencepark 904, 1098XH, Amsterdam")
+    postbox_address_api = models.CharField(_("API PO Box Address"), max_length=256, default="PO Box 94249, 1090 GE Amsterdam")
+    telephone_api = models.CharField(_("API Phone Number"), default="0031205257491", max_length=20)
+    webmaster_email_address = models.EmailField(_("API Webmaster e-mail address"), default="secr-astro-science@uva.nl")
 
-	class Meta:
-		verbose_name = 'API contact info'
-		verbose_name_plural = 'API contact info'
+    class Meta:
+        verbose_name = "API contact info"
+        verbose_name_plural = "API contact info"
+
+    def clean(self):
+        validate_only_one_instance(self)
