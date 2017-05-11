194777a7d83ab03bcf8c3cb46f1e4af99e9717e9
sepulta <davidhendriks93@gmail.com>
2017-05-12 01:16:14 +0200

    Changed plural of category

--- apiweb/apps/interviews/models.py
+++ apiweb/apps/interviews/models.py
@@ -43,6 +43,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Post(models.Model):
     author          = models.ForeignKey('auth.User', default=None)
