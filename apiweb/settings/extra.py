304dd3fa18c9e4a7d658f2775ff654ac5fb6fd5c
Timo Halbesma <timo@thNL.local>
2017-05-11 17:05:43 +0200

    Default file permissions after upload

--- apiweb/settings/extra.py
+++ apiweb/settings/extra.py
@@ -11,42 +11,50 @@ ADMIN_MEDIA_JS = (
 )
 
 FILEBROWSER_SHOW_IN_DASHBOARD = True
+FILEBROWSER_DEFAULT_PERMISSIONS = 0o644
+FILEBROWSER_EXTENSIONS = {
+    'Image': ['.jpg','.jpeg','.gif','.png','.tif','.tiff'],
+    'Document': [], # ['.pdf','.doc','.rtf','.txt','.xls','.csv'],
+    'Video': [], # ['.mov','.wmv','.mpeg','.mpg','.avi','.rm'],
+    'Audio': [], # ['.mp3','.mp4','.wav','.aiff','.midi','.m4p']
+}
+FILEBROWSER_ADMIN_VERSIONS = ['big'] # 'thumbnail', 'small', 'medium', 'large'
 
 # TINYMCE_SPELLCHECKER = True
 TINYMCE_COMPRESSOR = True
 TINYMCE_FILEBROWSER = True
 # https://www.tinymce.com/docs/demo/full-featured/
 TINYMCE_DEFAULT_CONFIG = {
-  'selector': 'textarea',
-  'height': 500,
-  'theme': 'modern',
-  'plugins': [
-    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-    'searchreplace wordcount visualblocks visualchars code fullscreen',
-    'insertdatetime media nonbreaking save table contextmenu directionality',
-    'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
-  ],
-  'toolbar1': 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-  'toolbar2': 'print preview image | forecolor backcolor emoticons | codesample',
-  'image_advtab': True,
-  'templates': [
-    { 'title': 'Test template 1', 'content': 'Test 1' },
-    { 'title': 'Test template 2', 'content': 'Test 2' }
-  ],
-  'content_css': [
-  ],
-}
+        'selector': 'textarea',
+        'height': 500,
+        'theme': 'modern',
+        'plugins': [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
+            ],
+        'toolbar1': 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        'toolbar2': 'print preview image | forecolor backcolor emoticons | codesample',
+        'image_advtab': True,
+        'templates': [
+            { 'title': 'Test template 1', 'content': 'Test 1' },
+            { 'title': 'Test template 2', 'content': 'Test 2' }
+            ],
+        'content_css': [
+            ],
+        }
 TINYMCE_MINIMAL_CONFIG = {
-    'mode': "",
-    'selector': 'textarea',
-    'height': 80,
-    'width': 500,
-    'menubar': False,
-    'statusbar': False,
-    'elementpath': False,
-    'plugins': [
-        'link paste autolink code',
-    ],
-    'toolbar1': 'undo redo | bold italic | bullist numlist outdent indent | link code',
-    'toolbar2': ''
-}
+        'mode': "",
+        'selector': 'textarea',
+        'height': 80,
+        'width': 500,
+        'menubar': False,
+        'statusbar': False,
+        'elementpath': False,
+        'plugins': [
+            'link paste autolink code',
+            ],
+        'toolbar1': 'undo redo | bold italic | bullist numlist outdent indent | link code',
+        'toolbar2': ''
+        }
