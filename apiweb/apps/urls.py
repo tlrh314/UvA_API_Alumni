9dace4eb75af7fbfc1d4d0701f7db56f8b3349d0
Timo Halbesma <timohalbesma@gmail.com>
2017-05-12 10:23:16 +0200

    Custom password reset template to include API contact info in case Alumnus does not know which email we have on file

--- apiweb/apps/urls.py
+++ apiweb/apps/urls.py
@@ -25,6 +25,11 @@ urlpatterns = [
     url(r'^ajax_select/', include(ajax_select_urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/password_reset/$', auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html',
+            extra_context = {
+                "api_phonenumber_formatted": contactinfo(None)["api_phonenumber_formatted"],
+                "secretary_email_address": contactinfo(None)["contactinfo"].secretary_email_address,
+            },
             email_template_name='registration/password_reset_email.html',
             extra_email_context = {
                 "api_phonenumber_formatted": contactinfo(None)["api_phonenumber_formatted"],
