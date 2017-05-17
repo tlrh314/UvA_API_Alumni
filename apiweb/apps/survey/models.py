51174d60fd446a3312ea8c39566afbf597b54bc2
Timo Halbesma <timohalbesma@gmail.com>
2017-05-17 22:46:00 +0200

    Survey Models

--- apiweb/apps/survey/models.py
+++ apiweb/apps/survey/models.py
@@ -5,35 +5,43 @@ import os.path
 from datetime import date
 
 from django.db import models
-# from django.db import IntegrityError
-# from django.urls import reverse
-# from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-# from django.template.defaultfilters import slugify
-# from django.core.exceptions import ObjectDoesNotExist
 from django.utils.encoding import python_2_unicode_compatible
 
-# from jsonfield import JSONField
-# from tinymce.models import HTMLField
-# from django_countries.fields import CountryField
+from tinymce.models import HTMLField
+from django_countries.fields import CountryField
 
-
+from ..main.models import validate_only_one_instance
 from ..alumni.models import Alumnus
 
 
+@python_2_unicode_compatible
+class SurveyText(models.Model):
+    intro_message  = HTMLField(verbose_name=_("Survey Description"), blank=True)
+    complete_message = HTMLField(verbose_name=_("Survey Complete Message"), blank=True)
+
+    class Meta:
+        verbose_name = _("Survey Explanation")
+        verbose_name_plural = _("Survey Explanation")
+
+    def clean(self):
+        validate_only_one_instance(self)
+
+
+@python_2_unicode_compatible
 class Sector(models.Model):
     name             = models.CharField(max_length=200)
-    slug             = models.SlugField(unique=True, blank=True, null=True)
 
     date_created     = models.DateTimeField(_("Date Created"), auto_now_add=True)
     date_updated     = models.DateTimeField(_("Date Last Changed"), auto_now=True)
-    last_updated_by  = models.ForeignKey('auth.User', related_name="sector_updated",
+    last_updated_by  = models.ForeignKey('auth.User', related_name="sectors_updated",
         on_delete=models.SET_DEFAULT, default=270)
 
     def __str__(self):
         return self.name
 
     class Meta:
+        verbose_name = "Job Sector"
         verbose_name_plural = "Job Sectors"
 
 
@@ -41,17 +49,12 @@ class Sector(models.Model):
 class JobAfterLeaving(models.Model):
     """ Represents a job after leaving API """
 
-    currently_occupating_job_choices = (
+    YES_OR_NO = (
         (1, "Yes"),
         (2, "No"),
     )
 
-    outside_inside_choices = (
-        (1, "Yes"),
-        (2, "No"),
-    )
-
-    location_job_choices = (
+    JOB_LOCATION_CHOICES = (
         (1, "NL"),
         (2, "Europe"),
         (3, "Great Bitain"),
@@ -60,13 +63,15 @@ class JobAfterLeaving(models.Model):
     )
 
     alumnus             = models.ForeignKey(Alumnus, related_name="job")
-    position_name       = models.CharField(_("position name"), blank=True, max_length=40)
-    current_job         = models.PositiveSmallIntegerField(_("current occupation"), choices=currently_occupating_job_choices, default=2)
-    company_name        = models.CharField(_("company name"), blank=True, max_length=40)
-    start_date          = models.DateField(_("date start job"), blank=True, null=True)
-    stop_date           = models.DateField(_("date stop job"), blank=True, null=True)
-    inside_academia     = models.PositiveSmallIntegerField(_("inside academia"), choices=outside_inside_choices, default=1)
-    location_job        = models.PositiveSmallIntegerField(_("location job"), choices=location_job_choices, default=1)
+    sector              = models.ForeignKey(Sector, blank=True, null=True)
+    company_name        = models.CharField(_("Company Name"), blank=True, max_length=100)
+    position_name       = models.CharField(_("Position Name"), blank=True, max_length=100)
+    is_current_job      = models.PositiveSmallIntegerField(_("Is Current"), choices=YES_OR_NO, default=1)
+    is_inside_academia  = models.PositiveSmallIntegerField(_("In Academia"), choices=YES_OR_NO, default=1)
+    # location_job        = models.PositiveSmallIntegerField(_("location job"), choices=JOB_LOCATION_CHOICES, default=1)
+    location_job        = CountryField(_("Location"), blank=True)
+    start_date          = models.DateField(_("From"), blank=True, null=True)
+    stop_date           = models.DateField(_("Until"), blank=True, null=True)
 
     comments            = models.TextField(_("comments"), blank=True)
     last_updated_by     = models.ForeignKey('auth.User', related_name="jobs_updated",
@@ -74,7 +79,6 @@ class JobAfterLeaving(models.Model):
     date_created        = models.DateTimeField(_("Date Created"), auto_now_add=True)
     date_updated        = models.DateTimeField(_("Date Last Changed"), auto_now=True)
 
-    sector = models.ForeignKey(Sector, blank=True, null=True)
 
 
 
