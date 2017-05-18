ad591ef453e6880c40effc06e4c4696a25fd46c8
Timo Halbesma <timohalbesma@gmail.com>
2017-05-18 14:50:48 +0200

    Example tree

--- apiweb/apps/visualization/urls.py
+++ apiweb/apps/visualization/urls.py
@@ -1,10 +1,11 @@
 from __future__ import unicode_literals, absolute_import, division
 
 from django.conf.urls import url
-from .views import tree
+from .views import tree, tree2
 
 
 urlpatterns = [
     url(r"^tree$", view=tree, name="tree"),
+    url(r"^tree2$", view=tree2, name="tree2"),
 ]
 
--- apiweb/apps/visualization/views.py
+++ apiweb/apps/visualization/views.py
@@ -5,6 +5,35 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView, RedirectView
 
+from ..alumni.models import Alumnus
+from ..alumni.models import Degree
 
 def tree(request):
+    phd_theses = Degree.objects.filter(type="phd").order_by("date_of_defence")
+    msc_theses = Degree.objects.filter(type="msc").order_by("date_of_defence")
+
+    # Create id for every
+    # for i, supervisor in enumerate(Alumnus.objects.filter(students__isnull=False).distinct()):
+
+
+    print('{\n  "nodes": [')
+    for i, supervisor in enumerate(Alumnus.objects.filter(students__isnull=False).distinct()):
+        print('    {"id": "'+supervisor.last_name+'", "group": '+str(i+1)+'},')
+    print('  ],\n  "links": [')
+
+    for thesis in phd_theses:
+        student = thesis.alumnus.last_name
+        for i, supervisor in enumerate(thesis.thesis_advisor.all()):
+            # print('{"id":'+'"{}", "{}"'.format(student, supervisor))
+            print('    {"source": "'+supervisor.last_name+'", "target": "'+student+'", "value": '+str(i+1)+'},')
+    print('  ]\n}')
+
+
+
+
+
     return render(request, "visualization/tree.html")
+
+
+def tree2(request):
+    return render(request, "visualization/tree2.html")
