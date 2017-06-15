38d1d587be06b36fe49e14f73e4e6b3198d87f26
sepulta <davidhendriks93@gmail.com>
2017-06-15 11:30:06 +0200

    changed some visualisation stuffs

--- apiweb/apps/visualization/urls.py
+++ apiweb/apps/visualization/urls.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals, absolute_import, division
 
 from django.conf.urls import url
-from .views import tree, tree2
+from .views import tree, tree2, tree_msc
 
 
 urlpatterns = [
     url(r"^tree$", view=tree, name="tree"),
     url(r"^tree2$", view=tree2, name="tree2"),
+    url(r"^tree_msc$",view=tree_msc,name="tree_msc"),
 ]
 
--- apiweb/apps/visualization/views.py
+++ apiweb/apps/visualization/views.py
@@ -21,7 +21,7 @@ def tree(request):
     #Get all thesis, students and advisors for phd
     for thesis in phd_theses:
         student = thesis.alumnus
-                
+
         for i, supervisor in enumerate(thesis.thesis_advisor.all()):
             if not supervisor in all_people:
                 all_people.append(supervisor)
@@ -45,12 +45,66 @@ def tree(request):
     data_dict["nodes"] = nodes_list
     data_dict["links"] = links_list
 
-    with open('json_content_phd.json', 'w') as outfile:
-        json.dump(data_dict, outfile)
+    #with open('json_content_phd.json', 'w') as outfile:
+    #    json.dump(data_dict, outfile)
 
     json_data = json.dumps(data_dict)
 
     return render(request, "visualization/tree.html", {'json_data': json_data})
+
+
+def tree_msc(request):
+    phd_theses = Degree.objects.filter(type="phd").order_by("date_of_defence")
+    msc_theses = Degree.objects.filter(type="msc").order_by("date_of_defence")
+
+    data_dict = {}
+    links_list, nodes_list, all_people = [], [], []
+
+    #Get all thesis, students and advisors for phd
+    for thesis in msc_theses:
+        student = thesis.alumnus
+        amt_supervisors = 0
+
+        for i, supervisor in enumerate(thesis.thesis_advisor.all()):
+            if not supervisor in all_people:
+                all_people.append(supervisor)
+            
+            link_dict = {}
+            link_dict["source"] = str(supervisor.last_name.replace("'"," "))
+            link_dict["target"] = str(student.last_name.replace("'"," "))
+            link_dict["value"] = i+1
+            links_list.append(link_dict)
+
+            amt_supervisors = i+1
+
+        #With the first condition, we skip all those who have not yet a supervisor assigned to them
+        if not amt_supervisors == 0:
+            if not student in all_people:
+                all_people.append(student)
+
+    #Get all the nodes
+    for alumnus in all_people:
+        node_dict = {}
+        node_dict["id"] = str(alumnus.last_name.replace("'"," "))
+        node_dict["group"] = i+1
+        nodes_list.append(node_dict)
+    
+    data_dict["nodes"] = nodes_list
+    data_dict["links"] = links_list
+
+    #with open('json_content_phd.json', 'w') as outfile:
+    #    json.dump(data_dict, outfile)
+
+    json_data = json.dumps(data_dict)
+
+    return render(request, "visualization/tree_msc.html", {'json_data': json_data})
+
+
+
+
+
+
+
 
 def tree2(request):
     return render(request, "visualization/tree2.html")
