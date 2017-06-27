ce19869fce404c12e5dd9529e7e5fcad9f8d3532
apialumni <info@api-alumni.nl>
2017-06-28 00:51:40 +0200

    Update to latest

--- apiweb/apps/research/actions.py
+++ apiweb/apps/research/actions.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from django.http import HttpResponse
 
 from .models import Thesis
+from ..alumni.models import Alumnus
 
 
 def save_all_theses_to_xls(request, queryset=None):
