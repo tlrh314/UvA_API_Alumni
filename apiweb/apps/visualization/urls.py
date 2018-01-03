4d0921022e9776524efa0dd9e6cc36ae55011fca
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2018-01-03 16:32:53 +0100

    switched to cytoscape.js

--- apiweb/apps/visualization/urls.py
+++ apiweb/apps/visualization/urls.py
@@ -7,6 +7,6 @@ from .views import tree, tree2, tree_msc
 urlpatterns = [
     url(r"^tree$", view=tree, name="tree"),
     url(r"^tree2$", view=tree2, name="tree2"),
-    url(r"^tree_msc$",view=tree_msc,name="tree_msc"),
+    url(r"^tree_msc$",view=tree_msc,name="tree_msc")
 ]
 
