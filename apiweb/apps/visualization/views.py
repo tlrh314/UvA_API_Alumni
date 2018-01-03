e08b43f829d2c2c9b7d00a11fe227cb9f02e5866
sepulta <davidhendriks93@gmail.com>
2018-01-03 22:04:14 +0100

    Provided views for visualisation

--- apiweb/apps/visualization/views.py
+++ apiweb/apps/visualization/views.py
@@ -1,6 +1,5 @@
-
-
 from django.http import Http404
+from django.db.models import Count
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView, RedirectView
@@ -9,7 +8,159 @@ import json
 
 from ..alumni.models import Alumnus
 from ..research.models import Thesis
+from ..survey.models import JobAfterLeaving
 
+
+def view_a(request):
+    """
+    View to display whether the 1,2,3rd job outside api is within or outside the field of astronomy
+    """
+
+    # jobs_outside = 
+    # jobs_inside = 
+
+    # jobs_cur = JobAfterLeaving.objects.filter(which_position=0)
+    # jobs_1 = JobAfterLeaving.objects.filter(which_position=1)
+    # jobs_2 = JobAfterLeaving.objects.filter(which_position=2)
+    # jobs_3 = JobAfterLeaving.objects.filter(which_position=3)
+
+
+
+
+    # json_data = json.dumps(data_dict)
+    # print(json_data)
+
+
+
+    return render(request, "visualization/vis_a.html", {'json_data': json_data})
+
+def view_b(request):
+    """
+    View to the location of the 1,2,3rd job (NL, eu..)
+    """
+    
+    data_dict, data_dict_cur, data_dict_1, data_dict_2, data_dict_3 = {}, {}, {}, {}, {}
+
+    fieldname = 'location_job'
+    jobs_all = JobAfterLeaving.objects.all() 
+    jobs_with_location = jobs_all.exclude(location_job='')
+    
+    jobs_cur = jobs_with_location.filter(which_position=0)
+    jobs_1 = jobs_with_location.filter(which_position=1)
+    jobs_2 = jobs_with_location.filter(which_position=2)
+    jobs_3 =jobs_with_location.filter(which_position=3)
+
+    location_counts_cur = jobs_cur.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in location_counts_cur:
+        data_dict_cur[el[fieldname]] = el['amount']
+
+    location_counts_1 = jobs_1.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in location_counts_1:
+        data_dict_1[el[fieldname]] = el['amount']
+    
+    location_counts_2 = jobs_2.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in location_counts_2:
+        data_dict_2[el[fieldname]] = el['amount']
+    
+    location_counts_3 = jobs_cur.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in location_counts_3:
+        data_dict_3[el[fieldname]] = el['amount']
+
+    data_dict['jobs_cur'] = data_dict_cur
+    data_dict['jobs_1'] = data_dict_1
+    data_dict['jobs_2'] = data_dict_2
+    data_dict['jobs_3'] = data_dict_3
+    #print data_dict
+
+    json_data = json.dumps(data_dict)
+    return render(request, "visualization/vis_f.html", {'json_data': json_data})
+
+def view_c(request):
+    """
+    View to visualize, if the job is in the field of astronomy, whether it is at a university, or a different institute type
+    """
+    pass
+
+def view_d(request):
+    """
+    View to visualize, if not inside field of astronomy, which sector the job is in.
+    """
+    data_dict, data_dict_cur, data_dict_1, data_dict_2, data_dict_3 = {}, {}, {}, {}, {}
+
+    fieldname = 'sector__name'
+    jobs_all = JobAfterLeaving.objects.all() 
+    jobs_with_sector = jobs_all.exclude(sector=None)
+    
+    sector_cur = jobs_with_sector.filter(which_position=0)
+    sector_1 = jobs_with_sector.filter(which_position=1)
+    sector_2 = jobs_with_sector.filter(which_position=2)
+    sector_3 = jobs_with_sector.filter(which_position=3)
+
+    sector_counts_cur = sector_cur.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in sector_counts_cur:
+        data_dict_cur[el[fieldname]] = el['amount']
+
+    sector_counts_1 = sector_1.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in sector_counts_1:
+        data_dict_1[el[fieldname]] = el['amount']
+    
+    sector_counts_2 = sector_2.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in sector_counts_2:
+        data_dict_2[el[fieldname]] = el['amount']
+    
+    sector_counts_3 = sector_3.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in sector_counts_3:
+        data_dict_3[el[fieldname]] = el['amount']
+
+    data_dict['sector_cur'] = data_dict_cur
+    data_dict['sector_1'] = data_dict_1
+    data_dict['sector_2'] = data_dict_2
+    data_dict['sector_3'] = data_dict_3
+    print (data_dict)
+
+    json_data = json.dumps(data_dict)
+    return render(request, "visualization/vis_f.html", {'json_data': json_data})
+
+def view_e(request):
+    """
+    What is the land of origin of the alumni
+    """
+
+    data_dict = {}
+    fieldname = 'country'
+
+    alumni = Alumnus.objects.all()    
+    alumni_with_country = alumni.exclude(country='')
+    
+    country_counts = alumni_with_country.values(fieldname).order_by(fieldname).annotate(amount=Count(fieldname))
+    for el in country_counts:
+        data_dict[el['country']] = el['amount']
+    json_data = json.dumps(data_dict)
+    return render(request, "visualization/vis_f.html", {'json_data': json_data})
+
+def view_f(request):
+    """
+    What is the gender of the alumnus?
+    """
+
+    data_dict = {}
+
+    alumni = Alumnus.objects.all()
+    alumni_with_gender = alumni.exclude(gender=None)
+    alumni_male = alumni_with_gender.filter(gender=1)
+    alumni_female = alumni_with_gender.filter(gender=2)
+    alumni_other = alumni_with_gender.filter(gender=3)
+    alumni_not_say = alumni_with_gender.filter(gender=4)
+
+    data_dict['male'] = alumni_male.count()
+    data_dict['female'] = alumni_female.count()
+    data_dict['other'] = alumni_other.count()
+    data_dict['rather_not_say'] = alumni_not_say.count()
+
+    json_data = json.dumps(data_dict)
+    return render(request, "visualization/vis_f.html", {'json_data': json_data})
+
+######
 
 def tree(request):
     # phd_theses = Thesis.objects.filter(type="phd").order_by("date_of_defence")
