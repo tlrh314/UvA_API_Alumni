1320e1e295e37b6a60db8729a5b094e4241e42cb
tlrh314 <timo@halbesma.com>
2020-09-11 15:33:18 +0200

    Do not run pre-commit hooks in settings/__init__.py

--- apiweb/settings/__init__.py
+++ apiweb/settings/__init__.py
@@ -1,7 +1,9 @@
-from django.core.files.storage import FileSystemStorage
+from .base import * 
+
+# NB, Load the filebrowser settings after base to ensure SECRET_KEY is set 
 from filebrowser.sites import site
-
-from .base import *
-
-site.storage = FileSystemStorage(location=STATIC_ROOT, base_url=STATIC_URL)
-site.directory = "img/"
+from django.core.files.storage import FileSystemStorage
+site.storage = FileSystemStorage(
+    location=STATIC_ROOT,
+    base_url=STATIC_URL
+)
