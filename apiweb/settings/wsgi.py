970c3155c6cc279f86d3c31ba81a1bdb457a5d48
tlrh314 <timo@halbesma.com>
2021-04-10 13:20:40 +0200

    Undo unwanted comment

--- apiweb/settings/wsgi.py
+++ apiweb/settings/wsgi.py
@@ -6,7 +6,7 @@ from django.core.wsgi import get_wsgi_application
 
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 sys.path.insert(0, os.path.abspath(os.path.join(root_path, "src")))
-# sys.path.insert(0, os.path.abspath("/srv/apiweb/venv3/lib/python3.7/site-packages/"))
+sys.path.insert(0, os.path.abspath("/srv/apiweb/venv3/lib/python3.7/site-packages/"))
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "apiweb.settings"
 
